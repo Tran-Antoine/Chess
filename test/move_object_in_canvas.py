c091b6266029607af3161b59cdcead862c3fd6b3
William K <willi1maril@gmail.com>
2019-12-24 01:11:55 +0100

    Improvement of the file move_object_in_canvas

--- test/move_object_in_canvas.py
+++ test/move_object_in_canvas.py
@@ -7,20 +7,24 @@ import tkinter as tk
 
 class Test():
     def __init__(self, root):
+        self.root = root
         self.canvas = tk.Canvas(height=500, width=500, bg="blue")
         self.objet1 = self.canvas.create_rectangle(0, 0, 40, 40, fill="white")
         self.objet2 = self.canvas.create_rectangle(50, 50, 90, 90, fill="Green")
         self.objets =[self.objet1, self.objet2]
         self.canvas.pack()
         self.objet = None
-        root.bind("<ButtonPress>", self.selectObject)
-        root.bind("<ButtonRelease>", self.move)
+        self.root.bind("<ButtonPress>", self.selectObject)
+        self.root.bind("<ButtonRelease>", self.release)
 
     def move(self, event):
         if self.objet is not None:
             self.objet_coord = self.canvas.coords(self.objet)
             self.canvas.move(self.objet, event.x - self.objet_coord[0] - 20, event.y - self.objet_coord[1] - 20)
-            self.objet = None
+
+    def release(self, event):
+        self.root.unbind("<Motion>")
+        self.objet = None
 
     def selectObject(self, event):
         """
@@ -30,6 +34,9 @@ class Test():
             if objet_coords[0] < event.x < objet_coords[2] and objet_coords[1] < event.y < objet_coords[3]:
 
                 self.objet = objet
+
+        self.root.bind("<Motion>", self.move)
+
 root = tk.Tk()
 Test(root)
 root.mainloop()
